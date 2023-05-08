1318b04746e288fa90439f84035bf33362124951
Sergej <129406138+Tumba123@users.noreply.github.com>
2023-05-08 23:58:20 +0200

    Portfolio 2.0
    New and improved site logic.

--- app.js
+++ app.js
@@ -1,25 +1,57 @@
-    // Add a scroll-triggered effect to the hero section
-    window.addEventListener("scroll", function() {
-        const hero = document.querySelector(".hero");
-        const windowScroll = window.pageYOffset;
-        hero.style.opacity = 1 - windowScroll / 400;
-        hero.style.transform = "translateY(" + windowScroll / 2 + "px)";
-      });
-      
-      const programs = document.querySelectorAll('.program');
+document.addEventListener('DOMContentLoaded', () => {
+    const blob = document.getElementById("blob");
+    const elements = document.querySelectorAll('.fade-in');
 
-      function showPrograms() {
-        programs.forEach(program => {
-          const position = program.getBoundingClientRect().top;
-          const screenPosition = window.innerHeight / 1.3;
-      
-          if (position < screenPosition) {
-            program.classList.add('show');
-          } else {
-            program.classList.remove('show');
-          }
-        });
-      }
-      
-      window.addEventListener('scroll', showPrograms);
-      
+    document.body.onpointermove = event => {
+        const { clientX, clientY } = event;
+
+        blob.animate(
+            [
+                { left: `${blob.style.left}`, top: `${blob.style.top}` },
+                { left: `${clientX}px`, top: `${clientY}px` }
+            ],
+            { duration: 3000, fill: "forwards" }
+        );
+    };
+
+    elements.forEach((element) => {
+        element.classList.add('fade-in');
+    });
+});
+
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const enhance = id => {
+  const element = document.getElementById(id),
+        text = element.innerText.split("");
+  
+  element.innerText = "";
+  
+  text.forEach((value, index) => {
+    const outer = document.createElement("span");
+    
+    outer.className = "outer";
+    
+    const inner = document.createElement("span");
+    
+    inner.className = "inner";
+    
+    inner.style.animationDelay = `${rand(-5000, 0)}ms`;
+    
+    const letter = document.createElement("span");
+    
+    letter.className = "letter";
+    
+    letter.innerText = value;
+    
+    letter.style.animationDelay = `${index * 1000 }ms`;
+    
+    inner.appendChild(letter);    
+    
+    outer.appendChild(inner);    
+    
+    element.appendChild(outer);
+  });
+}
+
+enhance("channel-link");
